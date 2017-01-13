b059ce97f17a4b8c4cfe7370d0d31d3126e896d4
Norton Pengra <npengra317@gmail.com>
2017-01-13 09:44:36 -0800

    put nullptr statements together #1

--- SinkTheFleet/fleet.cpp
+++ SinkTheFleet/fleet.cpp
@@ -105,9 +105,9 @@ void allocMem(Player players[], char size)
 		for (short playerIndex = 0; playerIndex < NUMPLAYERS; ++playerIndex)
 		{
 			// Setting the gameGride to a nullptr and then to an array (8 or 10) of ships
-			players[playerIndex].m_gameGrid[0] = nullptr; // Do we really need these lines?
+			players[playerIndex].m_gameGrid[0] = nullptr; 
+			players[playerIndex].m_gameGrid[1] = nullptr; 
 			players[playerIndex].m_gameGrid[0] = new Ship*[numberOfRows];
-			players[playerIndex].m_gameGrid[1] = nullptr; // Do we really need these lines?
 			players[playerIndex].m_gameGrid[1] = new Ship*[numberOfRows];
 			// iterate over all the rows
 			for (short rowIndex = 0; rowIndex < numberOfRows; ++rowIndex)
@@ -118,8 +118,8 @@ void allocMem(Player players[], char size)
 				// memory for each row in each grid
 				
 				players[playerIndex].m_gameGrid[0][rowIndex] = nullptr;
-				players[playerIndex].m_gameGrid[0][rowIndex] = new Ship[numberOfCols];
 				players[playerIndex].m_gameGrid[1][rowIndex] = nullptr;
+				players[playerIndex].m_gameGrid[0][rowIndex] = new Ship[numberOfCols];
 				players[playerIndex].m_gameGrid[1][rowIndex] = new Ship[numberOfCols];
 
 				// need to create an empty array in each row for each column
