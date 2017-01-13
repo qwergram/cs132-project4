5286e57dda924a89cae5ce6396aafdd7bc70dde7
Tabitha Roemish <tabitharoemish@gmail.com>
2017-01-13 11:00:45 -0800

    Completed memAlloc

--- SinkTheFleet/fleet.cpp
+++ SinkTheFleet/fleet.cpp
@@ -106,20 +106,18 @@ void allocMem(Player players[], char size)
 		{
 			// Setting the gameGride to a nullptr and then to an array (8 or 10) of ships
 			players[playerIndex].m_gameGrid[0] = nullptr;
+			players[playerIndex].m_gameGrid[1] = nullptr; 
 			players[playerIndex].m_gameGrid[0] = new Ship*[numberOfRows];
-			players[playerIndex].m_gameGrid[1] = nullptr;
 			players[playerIndex].m_gameGrid[1] = new Ship*[numberOfRows];
 			// iterate over all the rows
 			for (short rowIndex = 0; rowIndex < numberOfRows; ++rowIndex)
 			{
 				//-------------------------------------------------
-				//	your code goes here ...
-				// set the pointers to NULL, then allocate the
-				// memory for each row in each grid
+				players[playerIndex].m_gameGrid[0][rowIndex] = nullptr;
+				players[playerIndex].m_gameGrid[1][rowIndex] = nullptr;
+				players[playerIndex].m_gameGrid[0][rowIndex] = new Ship[numberOfCols];
+				players[playerIndex].m_gameGrid[1][rowIndex] = new Ship[numberOfCols];
 				
-
-				// need to create an empty array in each row for each column
-
 				//--------------------------------------------------
 				
 				// iterate over the columns and set them as no ship spots
