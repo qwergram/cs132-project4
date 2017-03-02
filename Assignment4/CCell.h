02346f965c0b83354cc6ba86f7e1b6457b10b98a
Norton Pengra <npengra317@gmail.com>
2017-03-02 09:51:19 -0800

    Create Accessor definitions

--- Assignment4/CCell.h
+++ Assignment4/CCell.h
@@ -13,11 +13,14 @@ namespace STFleet
 	{
 	public:
 		CCell(unsigned short row, unsigned short col); //Initializes m_row and m_col.
-		unsigned short getRow() const;
-		unsigned short getCol() const;
+		
 		void print(ostream& sout) const; //takes in an ostream reference. Prints out in the same format as in  project 1 (e.g. "A3" or "D12")
 		void print() const; // with no parameter.Prints to cout
 		bool inputCoordinates(istream& sin, char size) const; //takes in an istream reference and a char ('S' or 'L'). Inputs and validates the coordinates.
+
+		// Accessors
+		unsigned short getRow() const { return this->m_row; };
+		unsigned short getCol() const { return this->m_col; };
 
 	private:
 		unsigned short  m_row;
