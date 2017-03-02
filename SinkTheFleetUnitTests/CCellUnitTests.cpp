343b486c042b616f8237057b500984563d59f8f7
Norton Pengra <npengra317@gmail.com>
2017-03-02 09:54:57 -0800

    create constructor test

--- SinkTheFleetUnitTests/CCellUnitTests.cpp
+++ SinkTheFleetUnitTests/CCellUnitTests.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include "..\Assignment4\CCell.h"
+// #define private public IS NOT required for these tests
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -10,10 +11,13 @@ namespace SinkTheFleetUnitTests
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		// Test Construct assigns Row and Col correctly
+		TEST_METHOD(TestCCellConstructor)
 		{
-			
-			// TODO: Your test code here
+			STFleet::CCell * temp = new STFleet::CCell(5, 2);
+			Assert::AreEqual((unsigned short)5, temp->getRow());
+			Assert::AreEqual((unsigned short)2, temp->getCol());
+			delete temp;
 		}
 
 	};
