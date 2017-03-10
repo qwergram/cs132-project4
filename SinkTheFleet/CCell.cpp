1ac21227b82dfd34363d8b9f7d9c40b2530e560c
Norton Pengra <npengra317@gmail.com>
2017-03-10 14:13:12 -0800

    remove std namespace inclusion in source file

--- SinkTheFleet/CCell.cpp
+++ SinkTheFleet/CCell.cpp
@@ -1,25 +1,25 @@
 #include "SinkTheFleet.h"
 #include "CCell.h"
 
-STFGame::CCell::operator string()
+STFGame::CCell::operator std::string()
 {
-	stringstream result;
+	std::stringstream result;
 	result << ('A' + this->m_row);
 	result << ('0' + this->m_col);
 	return result.str;
 }
 
-bool STFGame::CCell::inputCoordinates(istream & sin, char size)
+bool STFGame::CCell::inputCoordinates(std::istream & sin, char size)
 {
 	bool isValid = true;
 	char highChar = static_cast<char>(BOARD_ROWS - 1) + 'A';
 	char row = 'A';
 	short col = 0;
-	cout << "Row must be a letter from A to " << highChar
+	std::cout << "Row must be a letter from A to " << highChar
 		<< " and column must be  from 1 to " << BOARD_COLS << ": ";
 	if ((row = toupper(sin.get())) < 'A' || row > highChar)
 	{
-		cout << "Row must be a letter from A to " << highChar
+		std::cout << "Row must be a letter from A to " << highChar
 			<< " and column must be  from 1 to " << BOARD_COLS <<
 			": ";
 		isValid = false;
@@ -37,7 +37,7 @@ bool STFGame::CCell::inputCoordinates(istream & sin, char size)
 	return isValid;
 }
 
-ostream & STFGame::operator<<(ostream & sout, const CCell & cell)
+std::ostream & STFGame::operator<<(std::ostream & sout, const CCell & cell)
 {
 	cell.print(sout);
 	return sout;
