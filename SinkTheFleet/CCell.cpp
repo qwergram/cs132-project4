7ffa92539779ae4ac16657de852ffb4d17850794
Norton Pengra <npengra317@gmail.com>
2017-03-10 13:54:24 -0800

    fix name errors

--- SinkTheFleet/CCell.cpp
+++ SinkTheFleet/CCell.cpp
@@ -9,26 +9,22 @@ STFGame::CCell::operator string()
 	return result.str;
 }
 
-bool STFGame::CCell::inputCoordinates(istream & sin, char size) const
+bool STFGame::CCell::inputCoordinates(istream & sin, char size)
 {
 	bool isValid = true;
-	short numberOfRows = (toupper(size) == 'L') ? LARGEROWS : SMALLROWS;
-	short numberOfCols = (toupper(size) == 'L') ? LARGECOLS : SMALLCOLS;
-	char highChar = static_cast<char>(numberOfRows - 1) + 'A';
+	char highChar = static_cast<char>(BOARD_ROWS - 1) + 'A';
 	char row = 'A';
 	short col = 0;
-	CCell location = { 0, 0 };
-	col = 0;
 	cout << "Row must be a letter from A to " << highChar
-		<< " and column must be  from 1 to " << numberOfCols << ": ";
+		<< " and column must be  from 1 to " << BOARD_COLS << ": ";
 	if ((row = toupper(sin.get())) < 'A' || row > highChar)
 	{
 		cout << "Row must be a letter from A to " << highChar
-			<< " and column must be  from 1 to " << numberOfCols <<
+			<< " and column must be  from 1 to " << BOARD_COLS <<
 			": ";
 		isValid = false;
 	}
-	else if (col < 1 || col > numberOfCols)
+	else if (col < 1 || col > BOARD_COLS)
 	{
 		sin >> col;
 		if (!sin)
@@ -36,8 +32,8 @@ bool STFGame::CCell::inputCoordinates(istream & sin, char size) const
 	}
 	else
 		isValid = false;
-	location.m_col = col - 1;
-	location.m_row = static_cast<short>(row - 'A');
+	this->m_col = col - 1;
+	this->m_row = static_cast<short>(row - 'A');
 	return isValid;
 }
 
